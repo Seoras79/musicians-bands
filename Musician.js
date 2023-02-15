e0e44667d7ce54d772496a50a086c0282a9828af
George Fleming <seorasproductions@gmail.com>
2023-02-15 17:19:09 +0000

    task completed

--- Musician.js
+++ Musician.js
@@ -1,7 +1,17 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+let Musician = sequelize.define('musician', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    instrument: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
 
 module.exports = {
     Musician
--- index.test.js
+++ index.test.js
@@ -12,13 +12,35 @@ describe('Band and Musician Models', () => {
         await sequelize.sync({ force: true });
     })
 
+    // test('can create a Band', async () => {
+    //     // TODO - test creating a band
+    //     expect('NO TEST').toBe('EXPECTED VALUE HERE');
+    // })
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const band = await Band.create({
+            name: 'The Beatles',
+            genre: 'Rock',
+            active: true
+        });
+        expect(band.name).toBe('The Beatles');
+        expect(band.genre).toBe('Rock');
     })
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+     // test('can create a Musician', async () => {
+    //     // TODO - test creating a musician
+    //     expect('NO TEST').toBe('EXPECTED VALUE HERE');
+    // })
+    test("can create a musician", async () => {
+        const musician = await Musician.create({
+            name: 'John Lennon',
+            instrument: 'Guitar',
+        });
+        expect(musician.name).toBe('John Lennon');
+        expect(musician.instrument).toBe('Guitar');
     })
+
+    
+
+   
 })
