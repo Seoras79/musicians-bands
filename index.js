61b87f447b96b7d9dd6cb088e0595ec1340d933a
George Fleming <seorasproductions@gmail.com>
2023-02-16 15:27:42 +0000

    defined associations and added tests for them

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
 
+// TODO - define the association between Band and Musician
+Band.hasMany(Musician);
+Musician.belongsTo(Band);
+
+
 module.exports = {
     Band,
     Musician
--- index.test.js
+++ index.test.js
@@ -21,10 +21,10 @@ describe('Band and Musician Models', () => {
         const band = await Band.create({
             name: 'The Beatles',
             genre: 'Rock',
-            active: true
+            
         });
-        expect(band.name).toBe('The Beatles');
-        expect(band.genre).toBe('Rock');
+        expect(band.name).toBe('The Beatles'); // check the name
+        expect(band.genre).toBe('Rock'); // check the genre
     })
 
      // test('can create a Musician', async () => {
@@ -32,12 +32,44 @@ describe('Band and Musician Models', () => {
     //     expect('NO TEST').toBe('EXPECTED VALUE HERE');
     // })
     test("can create a musician", async () => {
-        const musician = await Musician.create({
+        const musician = await Musician.create({ // create a musician
             name: 'John Lennon',
             instrument: 'Guitar',
         });
-        expect(musician.name).toBe('John Lennon');
-        expect(musician.instrument).toBe('Guitar');
+        expect(musician.name).toBe('John Lennon'); // check the name
+        expect(musician.instrument).toBe('Guitar'); // check the instrument
+    })
+    test("band has many musicians", async () => {
+        const band = await Band.create({ // create a band
+            name: 'The Beatles',
+            genre: 'Rock',
+        });
+        const musician = await Musician.create({ // create a musician
+            name: 'John Lennon',
+            instrument: 'Guitar'
+        });
+        const musician2 = await Musician.create({ // create a musician  
+            name: 'Paul McCartney',
+            instrument: 'Bass'
+        });
+       
+        await band.addMusicians([musician, musician2]);  // add multiple musicians
+        const musicians = await band.getMusicians(); // get all musicians
+        expect(musicians.length).toBe(2); // there should be 2 musicians
+        
+    })
+    test("musician belongs to a band", async () => { // test the association using async/await
+        const band = await Band.create({ // create a band
+            name: 'The Beatles',
+            genre: 'Rock',
+        });
+        const musician = await Musician.create({ // create a musician
+            name: 'John Lennon',
+            instrument: 'Guitar',
+        });
+        await band.addMusician(musician); // add a single musician
+        const musicians = await band.getMusicians(); // get all musicians
+        expect(musicians.length).toBe(1); // there should be 1 musician
     })
 
     
