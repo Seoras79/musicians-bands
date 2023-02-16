fbaab08a69b5f8364e78eb5349e0d4ad57a27bdb
George Fleming <seorasproductions@gmail.com>
2023-02-16 11:41:34 +0000

    forgot to use db.sqlite so have changed the code

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ const { Sequelize, Model } = require('sequelize');
 // TODO - create the new sequelize connection
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: path.join(__dirname, 'music.db')
-});
+    storage: path.join(__dirname, 'db.sqlite') 
+}); 
 
 module.exports = {
     sequelize,
